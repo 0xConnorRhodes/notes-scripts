f90df8fef043d570aa24a8611231e886f155f63a
Connor Rhodes <connor@rhodes.contact>
2024-06-01 20:54:40 +0000

    added functions to generate a list of notes linking to the done task

--- done-task.lua
+++ done-task.lua
@@ -5,7 +5,7 @@ elseif os.getenv("TERMUX_APP_PID") then
 end
 
 -- Function to get a list of files starting with ✅ in the specified directory
-function getFiles()
+local function getFiles()
     local files = {}
     for file in io.popen('ls ' .. notesPath .. '/✅*'):lines() do
         local fileName = file:match(".+/([^/]+)$")  -- Extract only the file name
@@ -23,6 +23,43 @@ local filesString = table.concat(files, '\n')
 -- Run fzf to let the user select a file
 local selectedFile = io.popen('echo "' .. filesString .. '" | fzf'):read()
 
+local function generateOriginalLink(input_string)
+    -- Remove the file extension from the string
+    local transformedString = input_string:gsub("%.md$", "")
+    
+    -- Surround the transformed string with [[ and ]]
+    local result = "[["..transformedString.."]]"
+    
+    return result
+end
+
+local function findTaskReferences(directory, pattern)
+    local files = {}
+    local p = io.popen('ls "'..directory..'"/*.md')
+
+    for file in p:lines() do
+        -- local grep_command = 'cat "'..file..'" | grep -q "'..pattern..'"'
+        local grep_command = 'grep -F -l -r -q "'..pattern..'" "'..file..'"'
+        if os.execute(grep_command) then
+            table.insert(files, file)
+        end
+    end
+
+    p:close()
+    return files
+end
+
+function moveDoneFile()
+    -- should return new relative path from the notes directory to the _done folder
+end
+
 print(selectedFile)
+
+local files = findTaskReferences(notesPath, generateOriginalLink(selectedFile))
+-- print(generateOriginalLink(selectedFile))
+
+for _, file in ipairs(files) do
+    print(file)
+end
 
 -- vim:syntax=lua
