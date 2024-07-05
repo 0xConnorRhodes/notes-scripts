9e683219b5767baa648d2cbc8eec7f7e6a75312a
Connor Rhodes <connor@rhodes.contact>
2024-07-05 18:28:49 +0000

    completed refactor with multi select and undo support

--- modify-task.lua
+++ modify-task.lua
@@ -85,41 +85,38 @@ elseif taskOperation == 'undone' or
     end
 -- #endregion
 
-for _, v in pairs(fileChoicesTbl) do
-    print(v)
-end
+-- #region rename references, move file
+    for _, task in pairs(fileChoicesTbl) do
+        local currentLinks = {}
+        currentLinks.noAlias = '[[_tk/'..operationFolder..'/'..task..']]'
+        currentLinks.withAlias = '[[_tk/'..operationFolder..'/'..task..'|'..task..']]'
+        local doneFilename = '✅ '..task:sub(8)
 
--- -- #region rename references, move file
---     for _, task in pairs(fileChoicesTbl) do
---         local currentLink = "[["..task.."]]"
---         local utcTime = os.time(os.date("!*t"))
---         local centralOffset = -6 * 3600
---         local centralTime = utcTime + centralOffset
---         local date = os.date("%y%m%d", centralTime)
---         local doneFilename = task:gsub("✅ ", date..'-')
+        for _, link in pairs(currentLinks) do
+            local grep_command = ('rg -F -l --color=never "%s" %s'):format(link, notesPath)
+            local handle = io.popen(grep_command)
+            local matchingFiles = handle:read("*a"):gsub('\n$','')
+            handle:close()
 
---         -- generate a file handle with files that contain the pattern (standard out from ripgrep)
---         local grep_command = ('rg -F -l --color=never "%s" %s'):format(currentLink, notesPath)
---         local handle = io.popen(grep_command)
---         local matchingFiles = handle:read("*a"):gsub('\n$','')
---         handle:close()
-        
---     for line in matchingFiles:gmatch("([^\n]+)") do
---         -- escape square brackets for sed command
---         local escapedStr = currentLink:gsub("%[", "\\["):gsub("%]", "\\]")
---         -- generate relative link to filed done task
---         local replacementStr = '\\[\\[_tk\\/'..taskOperation..'\\/'..doneFilename..'\\]\\]'
-        
---         local sedCmd = ("sed -i 's/%s/%s/g' '%s'"):format(escapedStr, replacementStr, line)
---         os.execute(sedCmd)
---     end
---     -- move file
---     local sourcePath = notesPath..'/'..task..'.md'
---     local destPath = notesPath..'/_tk/'..taskOperation..'/'..doneFilename..'.md'
---     local mvCmd = ("mv '%s' '%s'"):format(sourcePath, destPath)
---     os.execute(mvCmd)
---     print(taskOperation:upper()..': '..task)
---     end
--- -- #endregion
--- -- end elseif done|drop|hold
+            for line in matchingFiles:gmatch("([^\n]+)") do
+                -- escape square brackets for sed command
+                local escapedStr = link:gsub("%[", "\\["):gsub("%]", "\\]")
+                -- generate relative link to filed done task
+                local replacementStr = '\\[\\['..doneFilename..'\\]\\]'
+
+                local sedCmd = ("sed -i 's#%s#%s#g' '%s'"):format(escapedStr, replacementStr, line)
+                os.execute(sedCmd)
+            end
+        end
+
+        -- move file
+        local sourcePath = notesPath..'/_tk/'..operationFolder..'/'..task..'.md'
+        local destPath = notesPath..'/'..doneFilename..'.md'
+        local mvCmd = ("mv '%s' '%s'"):format(sourcePath, destPath)
+        os.execute(mvCmd)
+
+        print(taskOperation:upper()..': '..task)
+    end
+-- #endregion
+-- end elseif undone|undrop|unhold
 end
