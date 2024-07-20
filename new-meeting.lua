cae4b7a4aa10acaa229b681d5581ffb1eb93e314
Connor Rhodes <connor@rhodes.contact>
2024-07-20 17:00:58 +0000

    added check for Meetings header in parent note

--- new-meeting.lua
+++ new-meeting.lua
@@ -1,4 +1,4 @@
--- #region CONFIG
+--#region CONFIG
 local notesPath = ''
 if os.getenv('HOSTNAME') == 'devct' then
     notesPath = os.getenv("HOME")..'/notes'
@@ -12,9 +12,9 @@ local template = [[{{account_link}}
 # Context
 
 # Log]]
--- #endregion
+--#endregion
 
--- #region Generate Note Content
+--#region Generate Note Content
 local account = (
     io.popen(
         ('cat "%s" | fzf'):format(notesPath..'/.vaccounts.list')
@@ -30,25 +30,35 @@ local date = os.date("%y%m%d", os.time())
 io.write('desc: ')
 local description = io.read():gsub('%s$', '')
 
-local noteName = ('mt_%s %s %s.md'):format(date, account, description)
--- #endregion
+local noteName = ('mt_%s %s %s'):format(date, account, description)
+local noteNameExt = noteName..'.md'
+--#endregion
 
--- #region Render Template and Write Content
-local outputFilePath = notesPath..'/'..noteName
+--#region Render Template and Write Content
+local outputFilePath = notesPath..'/'..noteNameExt
 
 local renderedContent = template:gsub('{{account_link}}', '[['..account..']]')
 
-local file = io.open(outputFilePath, "w")
-if file then
-    file:write(renderedContent)
-    file:close()
-    print(noteName..' written successfully.')
+local noteFile = io.open(outputFilePath, "w")
+if noteFile then
+    noteFile:write(renderedContent)
+    noteFile:close()
+    print(noteNameExt..' written successfully.')
 else
     print("Error opening file for writing.")
 end
+--#endregion
 
 -- #region Add Link from Parent Note
--- TODO: check that the parent note has a heading named '^# Meetings'. If not, exit and warn
+local accountFile = account..'.md'
+local headerCheckCmd = ("rg '^# Meetings$' '%s'"):format(notesPath..'/'..accountFile)
+local headerCheckResult = io.popen(headerCheckCmd):read("*a")
+
+if #headerCheckResult > 0 then
+    print('header present')
+else
+    print('WARNING: Unable to add link to account file, Meetings header not present')
+end
 
 -- TODO: If '^# Meetings' is present then add a link to the note at the top of that heading
 
