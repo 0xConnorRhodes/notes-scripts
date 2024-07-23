1086eba5123e2bbd61ab20eba56c95c3aaff9238
Connor Rhodes <connor@rhodes.contact>
2024-07-23 00:04:56 +0000

    added open save file after creation

--- new-save.lua
+++ new-save.lua
@@ -12,8 +12,15 @@ local saveFilePath = savePath..'/'..saveFileName
 local testFile = io.open(saveFilePath, 'r')
 if not testFile then
     os.execute(string.format('touch "%s"', saveFilePath))
-    print('Created: '..saveFilePath)
 else
     print('Error: '..saveFileName..' already exists')
     testFile:close()
+end
+
+if Platform == 'Android' then
+    os.execute(string.format('termux-open "%s"', saveFilePath))
+elseif Platform == 'devct' then
+    os.execute(string.format('nvim -c "startinsert" "%s"', saveFilePath))
+else
+    print('Created: '..saveFilePath)
 end
