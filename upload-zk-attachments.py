66bc28f30d4c698805803659a4e5e27b820f1cae
Connor Rhodes <connor@rhodes.contact>
2024-07-24 22:54:07 +0000

    generate list of new local attachment files

--- upload-zk-attachments.py
+++ upload-zk-attachments.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+import os
+import glob
+import subprocess
+
+notes_dir = os.path.expanduser('~/notes')
+
+attachment_exts = [
+    'png',
+    'jpg'
+]
+
+attachment_files = []
+for ext in attachment_exts:
+    files = glob.glob(os.path.join(notes_dir, f"*.{ext}"))
+    attachment_files.extend(files)
+
+print(attachment_files)
