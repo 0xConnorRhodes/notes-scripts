325d3ec2ffeb4fa410124b425511f89610657072
Connor Rhodes <connor@rhodes.contact>
2024-08-09 01:46:18 +0000

    added tasks.py

--- aliases.fish
+++ aliases.fish
@@ -1,4 +1,4 @@
-alias tk="lua $HOME/code/notes-scripts/tasks.lua"
+alias tk="python3 $HOME/code/notes-scripts/tasks.py"
 alias ntk="lua $HOME/code/notes-scripts/new-task.lua"
 alias mt="lua $HOME/code/notes-scripts/new-meeting.lua"
 alias ns="$HOME/code/notes-scripts/notes-sync.sh"
---
+++ tasks.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+from pyfzf.pyfzf import FzfPrompt
+
+fzf = FzfPrompt()
+
+scripts_dir = os.path.expanduser('~/code/notes-scripts')
+
+options = {
+    "new task": f"lua {scripts_dir}/new-task.lua",
+    "list tasks": f"python3 {scripts_dir}/list-tasks.py",
+    "done task": f"lua {scripts_dir}/modify-task.lua done",
+    "drop task": f"lua {scripts_dir}/modify-task.lua drop",
+    "hold task": f"lua {scripts_dir}/modify-task.lua hold",
+    "review tasks": f"lua {scripts_dir}/review-tasks.lua",
+    "undone tasks": f"lua {scripts_dir}/modify-tasks.lua undone",
+    "undrop tasks": f"lua {scripts_dir}/modify-tasks.lua undrop",
+    "unhold tasks": f"lua {scripts_dir}/modify-tasks.lua unhold"
+}
+
+choice = fzf.prompt(options.keys())[0]
+
+subprocess.run(options[choice], shell=True)
+# print(choice)
