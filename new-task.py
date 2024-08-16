b2b15a88a2b72dc59dd78f50ccaa2df51524fefa
Connor Rhodes <connor@rhodes.contact>
2024-08-16 23:21:22 +0000

    added new-task.py with preamble code

--- new-task.py
+++ new-task.py
@@ -1,2 +1,16 @@
+import os
+import socket
+import subprocess
 from pyfzf.pyfzf import FzfPrompt
 fzf = FzfPrompt()
+
+notes_dir = os.path.expanduser('~/notes')
+
+hostname = socket.gethostname()
+termux_test = os.getenv('TERMUX_APP_PID')
+
+if hostname == 'devct':
+    platform = 'linux'
+elif int(termux_test) > 0:
+    platform = 'android'
+
