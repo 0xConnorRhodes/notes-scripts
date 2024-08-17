44c7c630b19f64d479b387e4c8e46231a6ae360a
Connor Rhodes <connor@rhodes.contact>
2024-08-17 02:58:38 +0000

    added function to split task and code to create task file

--- new-task.py
+++ new-task.py
@@ -1,6 +1,6 @@
+#region config
 import os
 import socket
-import subprocess
 from pyfzf.pyfzf import FzfPrompt
 fzf = FzfPrompt()
 
@@ -13,4 +13,28 @@ if hostname == 'devct':
     platform = 'linux'
 elif int(termux_test) > 0:
     platform = 'android'
+#endregion
 
+#region functions
+def split_task_sections(task_string):
+    """
+    split the input string into component parts
+    """
+    task_string = task_string.strip()
+    task_name = task_string
+    return task_name
+#endregion
+
+task_input = input('task: ')
+
+if not task_input:
+    print('no task')
+    exit(0)
+
+task_name = split_task_sections(task_input) # TODO: add, new, vars as you update split_task_sections()
+
+filename = f"tk_{task_name}.md"
+file = open(os.path.join(notes_dir, filename), 'w')
+file.close()
+
+# TODO: open task in nvim with `Gi` if devct else termux-open the note after creation
