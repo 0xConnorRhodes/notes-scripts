7c759f6a2af0d2a7f6a3a24ce78763bb947b64b9
Connor Rhodes <connor@rhodes.contact>
2024-08-24 15:12:39 +0000

    update lua version and fix typo

--- tasks.py
+++ tasks.py
@@ -9,13 +9,13 @@ scripts_dir = os.path.expanduser('~/code/notes-scripts')
 options = {
     "new task": f"python3 {scripts_dir}/new-task.py",
     "list tasks": f"python3 {scripts_dir}/list-tasks.py",
-    "done task": f"lua {scripts_dir}/modify-task.lua done",
-    "drop task": f"lua {scripts_dir}/modify-task.lua drop",
-    "hold task": f"lua {scripts_dir}/modify-task.lua hold",
+    "done task": f"lua5.4 {scripts_dir}/modify-task.lua done",
+    "drop task": f"lua5.4 {scripts_dir}/modify-task.lua drop",
+    "hold task": f"lua5.4 {scripts_dir}/modify-task.lua hold",
     # "review tasks": f"lua {scripts_dir}/review-tasks.lua",
-    "undone tasks": f"lua {scripts_dir}/modify-tasks.lua undone",
-    "undrop tasks": f"lua {scripts_dir}/modify-tasks.lua undrop",
-    "unhold tasks": f"lua {scripts_dir}/modify-tasks.lua unhold"
+    "undone tasks": f"lua5.4 {scripts_dir}/modify-task.lua undone",
+    "undrop tasks": f"lua5.4 {scripts_dir}/modify-task.lua undrop",
+    "unhold tasks": f"lua5.4 {scripts_dir}/modify-task.lua unhold"
 }
 
 choice = fzf.prompt(options.keys())[0]
