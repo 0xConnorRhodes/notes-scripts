2a764388dd895a9cc8a4bb15db4b5768fc1ca0f8
Connor Rhodes <connor@rhodes.contact>
2024-08-27 18:00:54 +0000

    added testing function, match statement, and loop to iterate over list

--- triage-tasks.py
+++ triage-tasks.py
@@ -5,13 +5,37 @@ fzf = FzfPrompt()
 notes_dir = os.path.expanduser("~/notes")
 # endregion
 
+
+import time
+def test_output():
+    print('---')
+    print(f'performing action **{chosen_action}**')
+    print(f'on:')
+    for file in selected:
+        print(f"  {file}")
+    time.sleep(3)
+
+
 # Get all files in the notes directory (not including subdirectories)
+# TODO: as the loop continues, show tk_files - processed files
 files = [f for f in os.listdir(notes_dir) if os.path.isfile(os.path.join(notes_dir, f))]
 tk_files = [f for f in files if f.startswith("tk_")]
+processed_files = []
 
-selected = fzf.prompt(tk_files, '--multi')
+while len(tk_files) > 0:
+    selected = fzf.prompt(tk_files, '--multi')
 
-if selected:
-    print("You selected:")
-    for file in selected:
-        print(f"{file}")
+    actions = [ 'drop', 'hold', 'done', 'skip' ]
+    chosen_action = fzf.prompt(actions)[0]
+
+    match chosen_action:
+        case 'drop':
+            test_output()
+        case 'hold':
+            test_output()
+        case 'done':
+            test_output()
+        case 'skip':
+            test_output()
+
+print(processed_files)
