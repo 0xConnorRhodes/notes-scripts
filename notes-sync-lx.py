992ca9b426b510fb30e507dc89241a9dc01780b9
Connor Rhodes <connor@rhodes.contact>
2024-09-07 10:11:14 -0500

    update notes-sync-lx.py to continue rebase if needed

--- notes-sync-lx.py
+++ notes-sync-lx.py
@@ -1,16 +1,30 @@
 import os
 import subprocess
+import runpy
 
 notes_dir = os.path.expanduser('~/notes')
 os.chdir(notes_dir)
 
 subprocess.run(['git', 'add', '.'], check=True)
 
-# Run git commit -m 'u'
-subprocess.run(['git', 'commit', '-m', 'u'], check=True)
+try:
+    result = subprocess.run(['git', 'commit', '-m', 'u'], 
+                            check=True,
+                            capture_output=True,
+                            text=True)
+except subprocess.CalledProcessError as e:
+    if 'git rebase --continue' in e.stdout:
+        subprocess.run("git rebase --continue", shell=True, check=True)
+
+    # if there is an error, but stdout didn't say to continue rebase, 
+    # then there's a different error that requires investigation
+    elif e.stderr:
+        print("ERROR: git threw error that git rebase --continue did not fix. Investigate")
 
 # Run git pull
 subprocess.run(['git', 'pull'], check=True)
 
 # Run git push
 subprocess.run(['git', 'push'], check=True)
+
+runpy.run_path(path_name=os.path.expanduser("~/code/notes-scripts/upload-zk-attachments.py"))
