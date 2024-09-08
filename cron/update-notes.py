47a89c1d4764ff374e7ca42313c76aee45b71bf8
Connor Rhodes <connor@rhodes.contact>
2024-09-08 08:09:55 -0500

    add update-notes.py

---
+++ cron/update-notes.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def check_and_clone():
+    notes_path = os.path.expanduser('~/notes')
+    
+    # Check if the folder exists
+    if not os.path.exists(notes_path):
+        print(f"Folder {notes_path} does not exist. Cloning repository...")
+        
+        # Clone the repository into the specified folder
+        clone_command = ['git', 'clone', 'git@github.com:0xConnorRhodes/notes-snapshot.git', notes_path]
+        
+        try:
+            subprocess.run(clone_command, check=True)
+            print("Repository cloned successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred while cloning the repository: {e}")
+    else:
+        print(f"Folder {notes_path} already exists.")
+
+if __name__ == "__main__":
+    check_and_clone()
