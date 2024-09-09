8f82dac05dd29fe2eae9c7b3f20c8765f21fb488
Connor Rhodes <connor@rhodes.contact>
2024-09-09 08:47:47 -0500

    update note-mainainance

--- cron/note-maintainance.sh
+++ cron/note-maintainance.sh
@@ -1,12 +1,14 @@
 #!/run/current-system/sw/bin/bash
 
+PYEXEC=/home/connor/code/notes-scripts/.pyenv/bin/python3
+
 echo "**Updating Notes**"
-python3 /home/connor/code/notes-scripts/cron/update-notes.py
+$PYEXEC /home/connor/code/notes-scripts/cron/update-notes.py
 
 echo ''
 echo "**Create Daily Note**"
-/home/connor/code/notes-scripts/.pyenv/bin/python3 create_daily_note.py
+$PYEXEC /home/connor/code/notes-scripts/cron/create_daily_note.py
 
 echo ''
 echo "**Sync Notes**"
-python3 /home/connor/code/notes-scripts/notes-sync-lx.py
+$PYEXEC /home/connor/code/notes-scripts/notes-sync-lx.py
