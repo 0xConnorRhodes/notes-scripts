bea1344d21eeaa1dad148cdf6219d8584550a77d
Connor Rhodes <connor@rhodes.contact>
2024-09-22 13:23:59 -0500

    update notesPath to be consistent across machines

--- modify-task.lua
+++ modify-task.lua
@@ -1,10 +1,11 @@
 -- #region CONFIG
-local notesPath = ""
-if os.getenv('HOSTNAME') == 'devct' then
-    notesPath = os.getenv("HOME")..'/notes'
-elseif os.getenv("TERMUX_APP_PID") then
-    notesPath = os.getenv("HOME")..'/storage/dcim/notes'
-end
+--local notesPath = ""
+local notesPath = os.getenv("HOME")..'/notes'
+--if os.getenv('HOSTNAME') == 'devct' then
+--    notesPath = os.getenv("HOME")..'/notes'
+--elseif os.getenv("TERMUX_APP_PID") then
+--    notesPath = os.getenv("HOME")..'/storage/dcim/notes'
+--end
 -- #endregion
 
 local taskOperation = arg[1]
