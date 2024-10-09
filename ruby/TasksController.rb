fa6d4ff5cf69cc78949a744c99f4fb6328db6fcf
Connor Rhodes <connor@rhodes.contact>
2024-10-08 22:23:27 -0500

    completed refactor of triage tasks functionality into a class

--- ruby/TasksController.rb
+++ ruby/TasksController.rb
@@ -24,68 +24,48 @@ class TasksController
       end
       @tasks = @tasks - future_tasks
     end
+  end
 
-    @tasks = @tasks.map {|f| File.basename(f)}.map {|i| i.sub('tk_', '')}.map {|i| i.sub('.md', '')}
-    @tasks.unshift('q')
+  def display
+    display_tasks = @tasks.map {|f| File.basename(f)}.map {|i| i.sub('tk_', '')}.map {|i| i.sub('.md', '')}
+    display_tasks.unshift('q') # add 'q' option at begining to allow quitting script
+
+    @chosen_tasks = fzf(display_tasks, "-m --preview='bat ~/notes/tk_{}.md --color=always --style=plain -l markdown'")
+    exit if @chosen_tasks[0] == 'q'
+
+    operation_list = ['done', 'drop', 'hold', 'q']
+    operation_list.unshift('edit') if @chosen_tasks.length == 1
+    @operation = fzf(operation_list)[0]
+    exit if @operation == 'q'
+  end
+
+  def process_choice
+    case @operation
+    when 'edit'
+      edit_file = File.join(@notes_folder, "tk_#{@chosen_tasks[0]}.md")
+      exec("nvim \"#{edit_file}\"")
+    when 'done', 'drop', 'hold'
+      @chosen_tasks.each do |task|
+        move_from = File.join(@notes_folder, "tk_#{task}.md")
+        move_to = File.join(@notes_folder, '_tk', @operation, "#{@date}-#{task}.md")
+      
+        FileUtils.mv(move_from, move_to)
+      
+        search_string = "tk_#{task}"
+        files_with_links = `rg -l -F -d 1 "[[#{search_string}]]" ~/notes`.split("\n")
+        replace_string = "_tk/#{@operation}/#{@date}-#{task}"
+      
+        files_with_links.each do |f|
+          `sed -i 's##{search_string}##{replace_string}#g' "#{f}"`
+        end
+      
+        puts "#{@operation.upcase}: #{task}"
+      end
+    end
   end
 end
 
 tasks = TasksController.new
 tasks.build_tasks_arr()
-
-exit
-##########################################
-
-
-
-chosen_tasks = fzf(tasks, "-m --preview='bat ~/notes/tk_{}.md --color=always --style=plain -l markdown'")
-exit if chosen_tasks[0] == 'q'
-
-operation_list = ['done', 'drop', 'hold', 'q']
-operation_list.unshift('edit') if chosen_tasks.length == 1
-operation = fzf(operation_list)[0]
-exit if operation == 'q'
-
-
-def refile_tasks(task_array, task_operation)
-  task_array.each do |task|
-    move_from = File.join(NOTES_FOLDER, "tk_#{task}.md")
-    move_to = File.join(NOTES_FOLDER, '_tk', task_operation, "#{date}-#{task}.md")
-
-    FileUtils.mv(move_from, move_to)
-
-    search_string = "tk_#{task}"
-    files_with_links = `rg -l -F -d 1 "[[#{search_string}]]" ~/notes`.split("\n")
-    replace_string = "_tk/#{task_operation}/#{date}-#{task}"
-
-    files_with_links.each do |f|
-      `sed -i 's##{search_string}##{replace_string}#g' "#{f}"`
-    end
-
-    puts "#{task_operation.upcase}: #{task}"
-  end
-end 
-
-if operation == 'edit'
-  edit_file = File.join(NOTES_FOLDER, "tk_#{chosen_tasks[0]}.md")
-  exec("nvim \"#{edit_file}\"")
-end
-
-#TODO: add tag functionality and factor this code into a file_task method
-refile_tasks(chosen_tasks, operation)
-# chosen_tasks.each do |task|
-#   move_from = File.join(notes_folder, "tk_#{task}.md")
-#   move_to = File.join(notes_folder, '_tk', operation, "#{date}-#{task}.md")
-
-#   FileUtils.mv(move_from, move_to)
-
-#   search_string = "tk_#{task}"
-#   files_with_links = `rg -l -F -d 1 "[[#{search_string}]]" ~/notes`.split("\n")
-#   replace_string = "_tk/#{operation}/#{date}-#{task}"
-
-#   files_with_links.each do |f|
-#     `sed -i 's##{search_string}##{replace_string}#g' "#{f}"`
-#   end
-
-#   puts "#{operation.upcase}: #{task}"
-# end
+tasks.display
+tasks.process_choice
