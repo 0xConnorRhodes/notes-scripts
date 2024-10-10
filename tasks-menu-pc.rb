925324ceaf8682d3b4f277b7f3235e1418cb063a
Connor Rhodes <connor@rhodes.contact>
2024-10-10 10:15:13 -0500

    ported tasks.py to tasks-menu-pc.rb

--- tasks-menu-pc.rb
+++ tasks-menu-pc.rb
@@ -7,11 +7,11 @@ options = {
   list_tasks: "python3 #{scripts_dir}/list-tasks.py",
   triage_tasks: "ruby #{scripts_dir}/triage-tasks.rb"
   done_task: "lua5.4 #{scripts_dir}/modify-task.lua done",
-  drop_task: "lua5.4 {scripts_dir}/modify-task.lua drop",
-  hold_task: "lua5.4 {scripts_dir}/modify-task.lua hold",
-  undone_tasks: "lua5.4 {scripts_dir}/modify-task.lua undone",
-  undrop_tasks: "lua5.4 {scripts_dir}/modify-task.lua undrop",
-  unhold_tasks: "lua5.4 {scripts_dir}/modify-task.lua unhold"
+  drop_task: "lua5.4 #{scripts_dir}/modify-task.lua drop",
+  hold_task: "lua5.4 #{scripts_dir}/modify-task.lua hold",
+  undone_tasks: "lua5.4 #{scripts_dir}/modify-task.lua undone",
+  undrop_tasks: "lua5.4 #{scripts_dir}/modify-task.lua undrop",
+  unhold_tasks: "lua5.4 #{scripts_dir}/modify-task.lua unhold"
 }
 
 choice = fzf(options.keys)[0].to_sym
