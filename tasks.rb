1535e9d9a6d1a29df7631d5153cd12df7e1a4307
Connor Rhodes <connor@rhodes.contact>
2024-10-09 22:29:04 -0500

    update tasks.rb with list-tasks support

--- tasks.rb
+++ tasks.rb
@@ -4,7 +4,7 @@ scripts_dir = File.expand_path("~/code/notes-scripts")
 
 options = {
   "new task" => "python3 {scripts_dir}/new-task.py",
-  "list tasks" => "python3 {scripts_dir}/list-tasks.py",
+  "list tasks" => "python3 #{scripts_dir}/list-tasks.py",
   "triage tasks" => "ruby #{scripts_dir}/triage-tasks.rb"
 }
 
