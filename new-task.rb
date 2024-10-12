3e5aa152e418eb03b4a2bb5b8a53ffe1e9a44b5e
Connor Rhodes <connor@rhodes.contact>
2024-10-12 12:45:20 -0500

    convert process_task_str to a private method

--- new-task.rb
+++ new-task.rb
@@ -12,15 +12,23 @@ class TaskCreator
     task_str = 'new task s 241014'
   end
 
-  def process_task_str(task_str)
-    start_match = task_str.match(/s \d{6}/)
-    start_date = start_match ? start_match[0].slice(2..-1).to_i : nil
-    due_match = task_str.match(/d \d{6}/)
-    due_date = due_match ? due_match[0].slice(2..-1).to_i : nil
-    task_name = task_str.sub(start_match.to_s, '').sub(due_match.to_s, '').strip
+  def create_task_file task_str
+    task_data = process_task_str(task_str)
   end
-end
+
+  private
+    def process_task_str task_str
+      start_match = task_str.match(/s \d{6}/)
+      start_date = start_match ? start_match[0].slice(2..-1).to_i : nil
+      due_match = task_str.match(/d \d{6}/)
+      due_date = due_match ? due_match[0].slice(2..-1).to_i : nil
+      task_name = task_str.sub(start_match.to_s, '').sub(due_match.to_s, '').strip
+
+      task_data = { task_name: task_name, start_date: start_date, due_date: due_date }
+    end
+
+end # end Class
 
 new_task = TaskCreator.new
 task_str = new_task.get_task_str
-new_task.process_task_str(task_str)
+new_task.create_task_file(task_str)
