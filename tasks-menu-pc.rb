d0d6835b4dace18dbda13e5ada8ebb4a8c20e031
Connor Rhodes <connor@rhodes.contact>
2024-10-11 20:42:39 -0500

    fix missing comma

--- tasks-menu-pc.rb
+++ tasks-menu-pc.rb
@@ -5,7 +5,7 @@ scripts_dir = File.expand_path("~/code/notes-scripts")
 options = {
   new_task: "python3 {scripts_dir}/new-task.py",
   list_tasks: "python3 #{scripts_dir}/list-tasks.py",
-  triage_tasks: "ruby #{scripts_dir}/triage-tasks.rb"
+  triage_tasks: "ruby #{scripts_dir}/triage-tasks.rb",
   done_task: "lua5.4 #{scripts_dir}/modify-task.lua done",
   drop_task: "lua5.4 #{scripts_dir}/modify-task.lua drop",
   hold_task: "lua5.4 #{scripts_dir}/modify-task.lua hold",
