a648ca0554c65320aa7a38a6c7fc1b4b33767caa
Connor Rhodes <connor@rhodes.contact>
2024-10-12 15:25:32 -0500

    set all methods public to make method sequence in script clearer

--- new-task.rb
+++ new-task.rb
@@ -20,44 +20,43 @@ class TaskCreator
     # task_str = 'new task s 241014 d 241015'
   end
 
-  def create_task_file task_str
-    file_content = render_file_content(task_str)
+  def create_task_file file_content
     puts file_content
   end
 
-  private
-    def process_task_str task_str
-      start_match = task_str.match(/s \d{6}/)
-      start_date = start_match ? start_match[0].slice(2..-1).to_i : nil
-      due_match = task_str.match(/d \d{6}/)
-      due_date = due_match ? due_match[0].slice(2..-1).to_i : nil
-      task_name = task_str.sub(start_match.to_s, '').sub(due_match.to_s, '').strip
+  def process_task_str task_str
+    start_match = task_str.match(/s \d{6}/)
+    start_date = start_match ? start_match[0].slice(2..-1).to_i : nil
+    due_match = task_str.match(/d \d{6}/)
+    due_date = due_match ? due_match[0].slice(2..-1).to_i : nil
+    task_name = task_str.sub(start_match.to_s, '').sub(due_match.to_s, '').strip
 
-      task_data = { task_name: task_name, start_date: start_date, due_date: due_date }
+    task_data = { task_name: task_name, start_date: start_date, due_date: due_date }
+  end
+
+  def render_file_content task_data
+    # file_lines = ['# meta', "\n", '# info', "\n\n", '# mtks', '- [ ] ', "\n"]
+    file_lines = ["# meta\n", "\n", "# info\n", "\n\n", "# mtks\n", "- [ ] \n", "\n"]
+
+    if task_data[:start_date] == nil and task_data[:due_date] == nil
+      file_lines.insert(1, "- \n")
     end
 
-    def render_file_content task_str
-      task_data = process_task_str(task_str)
-      # file_lines = ['# meta', "\n", '# info', "\n\n", '# mtks', '- [ ] ', "\n"]
-      file_lines = ["# meta\n", "\n", "# info\n", "\n\n", "# mtks\n", "- [ ] \n", "\n"]
-
-      if task_data[:start_date] == nil and task_data[:due_date] == nil
-        file_lines.insert(1, "- \n")
-      end
-
-      if task_data[:due_date]
-        file_lines.insert(1, "- due_date: #{task_data[:due_date]}\n")
-      end
-
-      if task_data[:start_date]
-        file_lines.insert(1, "- start_date: #{task_data[:start_date]}\n")
-      end
-
-      file_lines.join()
+    if task_data[:due_date]
+      file_lines.insert(1, "- due_date: #{task_data[:due_date]}\n")
     end
+
+    if task_data[:start_date]
+      file_lines.insert(1, "- start_date: #{task_data[:start_date]}\n")
+    end
+
+    file_lines.join()
+  end
 
 end # end Class
 
 new_task = TaskCreator.new
 task_str = new_task.get_task_str
-new_task.create_task_file(task_str)
+task_data = new_task.process_task_str(task_str)
+file_content = new_task.render_file_content(task_data)
+new_task.create_task_file(file_content)
