5f86b2fdaaac1eaf075e877d171de0b4fe6701b8
Connor Rhodes <connor@rhodes.contact>
2024-10-14 22:29:12 -0500

    update path to new task

--- tasks-menu-phone.rb
+++ tasks-menu-phone.rb
@@ -3,7 +3,7 @@ require_relative "modules/ruby/fzf"
 scripts_dir = File.expand_path("~/code/notes-scripts")
 
 options = {
-  new_task: "python3 {scripts_dir}/new-task.py",
+  new_task: "ruby #{scripts_dir}/new-task.rb",
   list_tasks: "python3 #{scripts_dir}/list-tasks.py",
   triage_tasks: "ruby #{scripts_dir}/triage-tasks.rb"
 }
