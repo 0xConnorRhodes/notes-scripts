6cf8659e8166f4dd295fd5dfcf3da6d01616e9c8
Connor Rhodes <connor@rhodes.contact>
2024-10-23 15:08:28 -0500

    add note template and render template with heredoc

--- new-meeting.rb
+++ new-meeting.rb
@@ -1,15 +1,51 @@
+require 'highline'
+require 'date'
 require_relative 'modules/ruby/fzf'
-require 'pry'
 
 NOTES_FOLDER = File.join(File.expand_path('~'), 'notes')
+
+note_template = <<-TEMPLATE
+[[{{ meetand }}]]
+
+# context
+
+# folks
+- 
+
+# log
+- 
+
+TEMPLATE
+
+
+cli = HighLine.new
 
 def choose_meetand
   vaccounts_list = File.readlines(File.join(NOTES_FOLDER, '.vaccounts.list')).map(&:chomp)
   rdex_list = File.readlines(File.join(NOTES_FOLDER, '.rdex.list')).map(&:chomp)
   meetands = (vaccounts_list + rdex_list).uniq
-  chosen_meetand = fzf(meetands)
+  chosen_meetand = fzf(meetands)[0]
+end
+
+def write_file(meeting_with, meeting_purpose, template)
+  date = Date.today.strftime("%y%m%d").to_s
+  file_name = "mt_#{date} #{meeting_with} #{meeting_purpose}.md"
+  filepath = "#{NOTES_FOLDER}/#{file_name}"
+
+  template = template.gsub('{{ meetand }}', meeting_with)
+
+  File.open(filepath, 'w') do |f|
+    f.puts template
+  end
+  puts "wrote: #{file_name}"
 end
 
 meetand = choose_meetand()
 
-binding.pry
+if meetand == 'NEW'
+  meetand = cli.ask "meetand: "
+end
+
+purpose = cli.ask "purpose: "
+
+write_file(meetand, purpose, note_template)
