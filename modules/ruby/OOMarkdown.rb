8dc88934652563bdf3d2324a2c31c5bdaf197a09
Connor Rhodes <connor@rhodes.contact>
2024-10-25 19:58:11 -0500

    added append_to_heading() to write given lines at the end of the given heading in a Markdown file

--- modules/ruby/OOMarkdown.rb
+++ modules/ruby/OOMarkdown.rb
@@ -6,12 +6,17 @@ class OOMarkdown
   # append the append_lines array to this section of the file and write the full contents back to the file
     file_lines = File.readlines(file)
     segment_start = file_lines.index(heading)
+
+    unless segment_start
+      puts "Error: given heading is not in the file"
+      exit(1)
+    end
+
     heading_level = heading.match(/^(#+).*/)&.[](1)
     segment_end = nil
 
     file_lines[segment_start+1..-1].each do |line|
       if line[0..heading_level.length] == heading_level + " "
-        puts line
         segment_end = file_lines.index(line)
         break
       end
@@ -19,14 +24,14 @@ class OOMarkdown
     end
 
     new_file_lines = file_lines[..segment_end-1] + append_lines + ["\n"] + file_lines[segment_end..]
-    puts new_file_lines
+    File.open(file, 'w') { |file| file.puts new_file_lines }
   end
 end
 
-oom = OOMarkdown.new()
+# oom = OOMarkdown.new()
 
-note = File.expand_path('~/notes/dn_241025.md') 
-heading = "# Today I *get* to\n"
-# heading = "## my one thing\n"
+# note = File.expand_path('~/notes/dn_241025.md') 
+# heading = "# Today I *get* to\n"
+# # heading = "## my one thing\n"
 
-test = oom.append_to_heading(note, heading, %w(newline1 newline2 newline3))
+# test = oom.append_to_heading(note, heading, %w(newline1 newline2 newline3))
