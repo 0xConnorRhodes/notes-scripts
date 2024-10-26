ac016b9d1f98977ced1ab4c8647cf1496b4ee150
Connor Rhodes <connor@rhodes.contact>
2024-10-25 20:36:33 -0500

    add check for error if on deck heading already in file

--- cron/load_todays_tasks.rb
+++ cron/load_todays_tasks.rb
@@ -7,6 +7,7 @@ def build_on_deck_section(due_tasks_arr, due_soon_tasks_arr, start_tasks_arr)
   on_deck_section += ['### due'] + due_tasks_arr.map{|i| "- [[tk#{i}]]"} + ["\n"] if due_tasks_arr.length > 0
   on_deck_section += ['### due soon'] + due_soon_tasks_arr.map{|i| "- [[tk#{i}]]"} + ["\n"] if due_soon_tasks_arr.length > 0
   on_deck_section += ['### start'] + start_tasks_arr.map{|i| "- [[tk#{i}]]"} + ["\n"] if start_tasks_arr.length > 0
+  on_deck_section.unshift('## on deck') unless on_deck_section.empty?
 
   return on_deck_section
 end
@@ -18,6 +19,11 @@ oom = OOMarkdown.new
 today = Date.today.strftime('%y%m%d').to_i
 daily_note_path = File.join(notes_folder, "dn_#{today}.md")
 target_heading = "# Today I *get* to\n"
+
+if File.readlines(daily_note_path).grep("## on deck\n").length > 0
+  puts "On deck tasks already present in file dn_#{today}.md"
+  exit(1)
+end
 
 due_tasks = tasks.get_tasks_by_date("due_date: ", today)
 due_soon_tasks = tasks.get_tasks_by_date("due_date: ", today+3) - due_tasks
