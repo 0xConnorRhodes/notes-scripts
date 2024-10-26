c8b6b0674b660d4db8c45f22055d5aa38e327e64
Connor Rhodes <connor@rhodes.contact>
2024-10-26 08:51:20 -0500

    factor functions into class

--- new-meeting.rb
+++ new-meeting.rb
@@ -1,6 +1,7 @@
 require 'highline'
 require 'date'
 require_relative 'modules/ruby/fzf'
+require 'pry'
 
 NOTES_FOLDER = File.join(File.expand_path('~'), 'notes')
 
@@ -20,32 +21,36 @@ TEMPLATE
 
 cli = HighLine.new
 
-def choose_meetand
-  vaccounts_list = File.readlines(File.join(NOTES_FOLDER, '.vaccounts.list')).map(&:chomp)
-  rdex_list = File.readlines(File.join(NOTES_FOLDER, '.rdex.list')).map(&:chomp)
-  meetands = (vaccounts_list + rdex_list).uniq
-  chosen_meetand = fzf(meetands)[0]
-end
-
-def write_file(meeting_with, meeting_purpose, template)
-  date = Date.today.strftime("%y%m%d").to_s
-
-  # add space between meetand and meeting purpose if meeting purpose is supplied
-  meeting_purpose = ' ' + meeting_purpose if meeting_purpose.length > 0
-
-  file_name = "mt_#{date} #{meeting_with}#{meeting_purpose}.md"
-  filepath = "#{NOTES_FOLDER}/#{file_name}"
-
-  rendered_template = template.gsub('{{ meetand }}', meeting_with)
-
-  File.open(filepath, 'w') do |f|
-    f.puts rendered_template
+class NewMeeting
+  def choose_meetand
+    vaccounts_list = File.readlines(File.join(NOTES_FOLDER, '.vaccounts.list')).map(&:chomp)
+    @rdex_list = File.readlines(File.join(NOTES_FOLDER, '.rdex.list')).map(&:chomp)
+    meetands = (vaccounts_list + @rdex_list).uniq
+    chosen_meetand = fzf(meetands)[0]
   end
 
-  puts "wrote: #{file_name}"
+  def write_file(meeting_with, meeting_purpose, template)
+    date = Date.today.strftime("%y%m%d").to_s
+
+    # add space between meetand and meeting purpose if meeting purpose is supplied
+    meeting_purpose = ' ' + meeting_purpose if meeting_purpose.length > 0
+
+    file_name = "mt_#{date} #{meeting_with}#{meeting_purpose}.md"
+    filepath = "#{NOTES_FOLDER}/#{file_name}"
+
+    rendered_template = template.gsub('{{ meetand }}', meeting_with)
+
+    File.open(filepath, 'w') do |f|
+      f.puts rendered_template
+    end
+
+    puts "wrote: #{file_name}"
+  end
 end
 
-meetand = choose_meetand()
+meet = NewMeeting.new
+
+meetand = meet.choose_meetand()
 
 if meetand == 'NEW'
   meetand = cli.ask "meetand: "
@@ -53,4 +58,4 @@ end
 
 purpose = cli.ask "purpose: "
 
-write_file(meetand, purpose, note_template)
+meet.write_file(meetand, purpose, note_template)
