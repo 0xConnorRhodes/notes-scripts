a6a0b7f466a991912de2ffffab7fb57937341147
Connor Rhodes <connor@rhodes.contact>
2024-10-30 19:55:16 -0500

    complete modify task rewrite in ruby

--- tasks-menu-phone.rb
+++ tasks-menu-phone.rb
@@ -6,6 +6,12 @@ options = {
   new_task: "ruby #{scripts_dir}/new-task.rb",
   list_tasks: "ruby #{scripts_dir}/list-tasks.rb",
   triage_tasks: "ruby #{scripts_dir}/triage-tasks.rb"
+  done_task: "ruby #{scripts_dir}/modify-task.rb done",
+  drop_task: "ruby #{scripts_dir}/modify-task.rb drop",
+  hold_task: "ruby #{scripts_dir}/modify-task.rb hold",
+  undone_task: "ruby #{scripts_dir}/modify-task.rb undone",
+  undrop_task: "ruby #{scripts_dir}/modify-task.rb undrop",
+  unhold_task: "ruby #{scripts_dir}/modify-task.rb unhold"
 }
 
 choice = fzf(options.keys)[0].to_sym
