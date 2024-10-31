829c3545e012af0fedcb30b6718f4c70d618e9bc
Connor Rhodes <connor@rhodes.contact>
2024-10-30 20:19:03 -0500

    added oneshot script to refactor links for updated dir path

---
+++ oneshot/refactor_links.rb
@@ -0,0 +1,19 @@
+require 'find'
+
+directory = File.expand_path('~/notes')
+
+def replace_pattern(file_path)
+  content = File.read(file_path)
+  new_content = content.gsub('[[_tk', '[[t')
+
+  if content != new_content
+    File.write(file_path, new_content)
+    puts "Updated: #{file_path}"
+  end
+end
+
+files = Dir.glob("#{directory}/**/*.md")
+
+files.each do |f|
+  replace_pattern(f)
+end
