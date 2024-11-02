2e682d4d51f0ef224f4ff1270aace178e20bc4a4
Connor Rhodes <connor@rhodes.contact>
2024-11-02 12:52:09 -0500

    swap to output array

--- list-tasks.rb
+++ list-tasks.rb
@@ -1,6 +1,7 @@
 require 'date'
 require_relative 'modules/ruby/TaskLister'
 
+
 today = Date.today.strftime('%y%m%d').to_i
 tasks = TaskLister.new
 
@@ -8,17 +9,20 @@ due_tasks = tasks.get_tasks_by_date("due_date: ", today)
 due_soon_tasks = (tasks.get_tasks_by_date("due_date: ", today+2) - due_tasks)
 start_tasks = tasks.get_tasks_by_date("start_date: ", today) - (due_tasks + due_soon_tasks)
 
+output = []
 if due_tasks.length > 0
-  puts "due tasks:"
-  puts due_tasks
+  output << "due tasks:"
+  output += due_tasks
 end
 
 if due_soon_tasks.length > 0
-  puts "due soon tasks:"
-  puts due_soon_tasks
+  output << "due soon tasks:"
+  output += due_soon_tasks
 end
 
 if start_tasks.length > 0
-  puts "start tasks:"
-  puts start_tasks
+  output << "start tasks:"
+  output += start_tasks
 end
+
+puts output
