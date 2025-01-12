ea16a69ce9083b1e2dbe5dfb8152f05127f61db3
Connor Rhodes <connor@rhodes.contact>
2025-01-12 13:17:58 -0600

    complete initial version

--- cron/upload_attachments.rb
+++ cron/upload_attachments.rb
@@ -6,24 +6,20 @@ remote_dir = ENV['REMOTE_DIR']
 attachments = Dir.glob(File.join(notes_dir, 'zattachments', '*'))
 attachments = attachments.map {|f| File.basename(f)}
 
-# for each local_attachment, check if it is linked in the notes
-# requirements: check for relative and name link for wiki-link and markdown link
-count = 0
 attachments.each do |attachment|
-  count += 1
-  puts "Processing item #{count}"
-
   upload = false
   file_data = {
     wiki_link_files: `rg -F -l "zattachments/#{attachment}]]" "#{notes_dir}"`.split("\n"),
     md_link_files: `rg -F -l "zattachments/#{attachment})" "#{notes_dir}"`.split("\n"),
+    wiki_short_link_files: `rg -F -l "![[#{attachment}]]" "#{notes_dir}"`.split("\n"),
   }
 
   # skip dangling attachments
-  files = file_data[:wiki_link_files] + file_data[:md_link_files]
+  files = file_data[:wiki_link_files] + file_data[:md_link_files] + file_data[:wiki_short_link_files]
   if files.count == 0
-    # puts "No files found for #{attachment}"
-    # `rm "#{notes_dir}/zattachments/#{attachment}"`
+    present_files = `rg -F -l "#{attachment}" "#{notes_dir}"`.split("\n")
+    puts "No files found for #{attachment}, removing"
+    `rm "#{notes_dir}/zattachments/#{attachment}"` if present_files.count == 0
     next
   end
 
@@ -33,7 +29,7 @@ attachments.each do |attachment|
   file_data[:wiki_link_files].each do |file_path|
     file_content = File.read(file_path)
     # match any amount of ../ in relative path
-    pattern = /!\[\[(?:\.\.\/)*zattachments\/#{attachment}\]\]/
+    pattern = /!?\[\[(?:\.\.\/)*zattachments\/#{attachment}\]\]/
     matches = file_content.scan(pattern)
     matches.uniq.each do |match|
       puts "replacing #{match} in #{file_path}"
@@ -43,7 +39,30 @@ attachments.each do |attachment|
     end
   end
 
-  # TODO: logic for md links
+  file_data[:md_link_files].each do |file_path|
+    file_content = File.read(file_path)
+    # match any amount of ../ in relative path and any link text
+    pattern = /!?\[[^\]]*\]\((?:\.\.\/)*\/?zattachments\/#{attachment}\)/
+    matches = file_content.scan(pattern)
+    matches.uniq.each do |match|
+      puts "replacing #{match} in #{file_path}"
+      new_link = "![](#{ENV['STATIC_URL']}/#{remote_filename}?#{ENV['ACCESS_TOKEN']})"
+      File.write(file_path, file_content.gsub(match, new_link))
+      upload = true
+    end
+  end
+
+  file_data[:wiki_short_link_files].each do |file_path|
+    file_content = File.read(file_path)
+    # match any amount of ../ in relative path
+    search_string = "![[#{attachment}]]"
+    if file_content.include?(search_string)
+      puts "replacing #{search_string} in #{file_path}"
+      new_link = "![](#{ENV['STATIC_URL']}/#{remote_filename}?#{ENV['ACCESS_TOKEN']})"
+      File.write(file_path, file_content.gsub(search_string, new_link))
+      upload = true
+    end
+  end
 
   if upload
     puts "Uploading #{attachment}"
