28176c964be8a0519cfb5008662b10f69cea6361
Connor Rhodes <connor@rhodes.contact>
2025-01-13 17:28:58 -0600

    add git pull

--- notes-sync.rb
+++ notes-sync.rb
@@ -3,14 +3,13 @@
 notes_path = File.expand_path('~/notes')
 
 Dir.chdir(notes_path) do
-  # Add all changes
+  system('git pull')
+
   system('git add .')
 
-  # Commit with a message
   commit_message = "Update notes - #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
   system("git commit -m '#{commit_message}'")
 
-  # Push the changes
   system('git push')
 end
 
