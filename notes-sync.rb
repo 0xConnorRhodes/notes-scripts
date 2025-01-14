8421062cc550940ea5833de40a48d3d4785a9e25
Connor Rhodes <connor@rhodes.contact>
2025-01-13 22:11:56 -0600

    reorder git commands to commit local changes first

--- notes-sync.rb
+++ notes-sync.rb
@@ -6,17 +6,15 @@ require_relative 'modules/upload_attachments'
 
 $notes_path = File.expand_path('~/notes')
 
-require 'pry'
-
 upload_attachments
 
 Dir.chdir($notes_path) do
-  system('git pull')
-
   system('git add .')
 
   commit_message = "Update notes - #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
   system("git commit -m '#{commit_message}'")
+
+  system('git pull --rebase')
 
   system('git push')
 end
