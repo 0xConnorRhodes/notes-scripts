ea6ce8de4676d0866f3b58869d4238ee631b0810
Connor Rhodes <connor@rhodes.contact>
2025-02-17 20:04:52 -0600

    remove rename temp tasks

--- notes-sync.rb
+++ notes-sync.rb
@@ -3,11 +3,8 @@
 require 'dotenv/load'
 Dotenv.load(File.join(File.dirname(__FILE__), '.env'))
 require_relative 'modules/upload_attachments'
-require_relative 'modules/rename_temp_tasks'
 
 $notes_path = File.expand_path('~/notes')
-
-rename_temp_tasks
 
 unless system('ping -c 1 -W 1 1.1.1.1 > /dev/null 2>&1')
   puts "No internet connection"
